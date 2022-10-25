888d2877b3a1ea9226364de560ba168712c0042f
higuchiyumiko <marguerite.0501.hurihura.23@gmail.com>
2022-10-25 12:31:04 +0900

    バリデーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -18,7 +19,7 @@ public function index(Post $post)
  public function create(){
      return view('posts/create');
  }
- public function store(Request $request,Post $post){
+ public function store(PostRequest $request,Post $post){
      $input=$request['post'];
      $post->fill($input)->save();
      return redirect('/posts/'.$post->id);
