a3fe67ef1942da12f28c274aa4cd213148614728
higuchiyumiko <marguerite.0501.hurihura.23@gmail.com>
2022-10-25 09:39:05 +0900

    記事投稿作成画面

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use App\Http\Controllers\PostController;  //外部にあるPostControllerクラ�
 |
 */
 Route::get('/', [PostController::class,'index']);
+Route::get('/posts/create',[POstController::class,'create']);
 Route::get('/posts/{post}', [PostController::class,'show']);
